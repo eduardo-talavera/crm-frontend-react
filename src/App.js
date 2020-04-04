7a5611ee8d6e65e040ab87f8349c301aee580d9f
Kuteji <edutalavera@outlook.com>
2020-04-04 00:14:01 -0600

    modificando el app

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
         <Fragment>
           <CRMProvider value={[auth, guardarAuth]}>
             <Header/>
-        <div className="grid contenedor contenido-principal">
+        <div className="gri contenedor contenido-principal">
 
             <Navegacion/>
 
